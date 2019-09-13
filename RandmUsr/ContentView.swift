d3fb80c21d430260e3b995be49847bf8fc8826a5
Andras Samu <samu.andris1@gmail.com>
2019-09-13 22:12:45 +0200

    Initial Commit

---
+++ RandmUsr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandmUsr
+//
+//  Created by András Samu on 2019. 09. 13..
+//  Copyright © 2019. András Samu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
